14d4f1f655b0b93a180f456b027645f044d6cd34
Sigrid <sigridaasma@gmail.com>
2017-02-22 17:25:00 +0200

    Homework 1 first solution

---
+++ src/homework/hw1/ColorSort.java
@@ -0,0 +1,49 @@
+package homework.hw1;//Testing histogram example
+
+public class ColorSort {
+
+   public enum Color {
+       red, green, blue;
+   }
+
+   public static Color[] balls = null;
+   
+   public static void main (String[] param) {
+      // for debugging
+   }
+
+    public static void reorder(Color[] balls)
+    {
+        Color[] sortedBalls = new Color[balls.length];
+
+        int j2rg = 0;
+
+        for (int i = 0; i < balls.length; i++) {
+            if (balls[i] == Color.red) {
+                sortedBalls[j2rg] = Color.red;
+                j2rg ++;
+            }
+        }
+        for (int i = 0; i < balls.length; i++) {
+            if (balls[i] == Color.green) {
+                sortedBalls[j2rg] = Color.green;
+                j2rg ++;
+            }
+        }
+
+        for (int i = 0; i < balls.length; i++) {
+            if (balls[i] == Color.blue) {
+                sortedBalls[j2rg] = Color.blue;
+                j2rg ++;
+            }
+        }
+
+        // sorting magic happens here
+
+        System.arraycopy(sortedBalls, 0, balls, 0, balls.length);
+    }
+
+
+
+}
+
