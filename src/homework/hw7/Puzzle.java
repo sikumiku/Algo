c22f95eaa13f7ed1d7203f8bd8440c4fa2e33f46
Sigrid <sigridaasma@gmail.com>
2017-05-07 17:05:22 +0300

    HW7 first version not working yet

--- src/homework/hw7/Puzzle.java
+++ src/homework/hw7/Puzzle.java
@@ -11,7 +11,10 @@ import java.util.*;
 
 public class Puzzle {
 
+
    public static Set<String> set = new HashSet<>();
+    public static ArrayList<String> puzzlelist = new ArrayList<>();
+   public static int counter = 0;
 
    /** Solve the word puzzle.
     * @param args three words (addend1 addend2 sum)
@@ -29,13 +32,17 @@ public class Puzzle {
 
    public static void findSolutions (String[] sarray) {
 //      isValidPuzzle(sarray);
-      String puzzleString = convertToSingleString(sarray);
-      ArrayList<String> letters = createUniqueCharList(puzzleString);
-      System.out.println(letters.size());
+      String puzzleString = convertToSingleString(sarray); // SEND MORE MONEY -> SENDMOREMONEY
+      ArrayList<String> letters = createUniqueCharList(puzzleString); //S, E, N, D, M, O, R, Y
       if (!(isUsableCharList(letters))) {
          throw new RuntimeException("Too many letters to solve the puzzle.");
       }
-      createNumberVariations("0123456789", "", letters.size());
+      createNumberVariations("0123456789", "", letters.size()); //set now contains all possible 8 digit number combos
+      for (String s : set) { //iga 8 kohalise numbri kohta
+          replaceLettersWithNumbers(s.toCharArray(), letters); //S, E, N, D, M, O, R, Y -> 5,3,9,1,2,0,6,4
+          generateAnswersforPuzzle(puzzleString, sarray);
+      }
+
 
    }
 
@@ -74,6 +81,41 @@ public class Puzzle {
          }
          createNumberVariations(numbers.substring(0, i) + numbers.substring(i + 1), temp + number, numberOfLetters);
       }
+   }
+
+   public static void replaceLettersWithNumbers(char[] numbers, ArrayList<String> letters) {
+       int numbercounter = 0;
+       for (int i = 0; i < letters.size(); i++) {
+           letters.set(i, Character.toString(numbers[numbercounter]));
+           numbercounter++;
+       }
+       puzzlelist = letters;
+   }
+
+   public static void generateAnswersforPuzzle(String sinput, String[] puzzle) {
+       StringBuilder firstelement = new StringBuilder();
+       StringBuilder secondelement = new StringBuilder();
+       StringBuilder sum = new StringBuilder();
+
+       char[] chars = sinput.toCharArray();
+       int size1 = puzzle[0].length();
+       int size2 = puzzle[1].length();
+       int totalsize = sinput.length();
+       for (int i = 0; i < size1; i++) {
+           firstelement.append(puzzlelist.get(puzzlelist.indexOf(Character.toString(chars[i]))));
+       }
+       for (int i = size1; i < size1 + size2; i++) {
+           secondelement.append(puzzlelist.get(puzzlelist.indexOf(Character.toString(chars[i]))));
+       }
+       for (int i = size2; i < totalsize; i++) {
+           sum.append(puzzlelist.get(puzzlelist.indexOf(Character.toString(chars[i]))));
+       }
+       if (!(firstelement.toString().substring(0,1).equals("0") || secondelement.toString().substring(0,1).equals("0") || sum.toString().substring(0,1).equals("0"))) {
+           if ((Long.parseLong(firstelement.toString()) + Long.parseLong(secondelement.toString()) == Long.parseLong(sum.toString()))) {
+               System.out.println(firstelement + " + " + secondelement + " = " + sum);
+               counter++;
+           }
+       }
    }
 
 
